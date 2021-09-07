fe371ccb2e23aab60220105224c25b610d2fe76f
Yash <yashsethiya58@gmail.com>
2021-09-07 16:50:00 +0530

    Add Procfile and Update ports,gitignore

--- server.js
+++ server.js
@@ -35,4 +35,12 @@ app.get('/:shortUrl', async (req, res) => {
     res.redirect(shortUrl.full)
 })
 
-app.listen(process.env.PORT || 5000);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+
+//app.listen(process.env.PORT || 5000);
+app.listen(port,function(){
+    console.log("Server has started successfully");
+})
